2d61969a525e117564af847571b11ca719191ab2
andra-sun <camileguimaraes463@gmail.com>
2026-03-27 09:49:49 -0300

    borda do botao atualizada

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,7 +9,7 @@ function Button({ onClick }: ButtonProps) {
     return (
         <button
             onClick={onClick}
-            className="absolute top-4/5 left-5/6 bg-orange-400 p-4 rounded-lg border-b-15 border-r-9 border-orange-700 text-white font-black text-lg hover:border-b-[5px] hover:border-r-4 hover:bg-orange-500 hover:translate-y-2 active:translate-y-3 transition-all duration-150 shadow-md"
+            className="absolute top-4/5 left-5/6 bg-orange-400 p-4 rounded-lg border-b-9 border-r-8 rounded-b-sm border-orange-700 text-white font-black text-lg hover:border-b-5 hover:border-r-4 hover:bg-orange-500 hover:translate-y-2 active:translate-y-3 transition-all duration-150 shadow-md"
         >
             Sobre mim
         </button>
